ac4c8cc57d5a636ce2cde4b5d6a66cc9a909c077
devin007 <devin@007vin.com>
2021-03-16 15:18:27 +0800

    提交

---
+++ app/src/main/java/com/yunqipei/pp_widget_library/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yunqipei.pp_widget_library
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
